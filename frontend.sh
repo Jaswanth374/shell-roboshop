25c3eaf27dcd534825af4d3479edadcd067f7881
Jaswanth374 <jaswanthmarpuri@gmail.com>
2026-02-01 08:14:00 +0530

    push the script to repo

--- frontend.sh
+++ frontend.sh
@@ -7,11 +7,13 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+NGINX="/etc/nginx/nginx.conf"
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
     exit 1
 fi
+
 
 mkdir -p $LOGS_FOLDER
 
@@ -24,6 +26,12 @@ VALIDATE(){
     fi
 }
 
+if [ -f $NGINX ]; then
+   echo -e "$Y Existing nginx config found. Removing... $N" | tee -a $LOGS_FILE
+  rm -rf "$NGINX" &>>$LOGS_FILE
+  VALIDATE $? "Remove existing nginx config"
+fi
+
 dnf module list nginx &>>$LOGS_FILE
 VALIDATE $? "NodeJs module list" 
 
@@ -32,7 +40,7 @@ VALIDATE $? "Disable nginx modules"
 
 dnf module enable nginx:1.24 -y &>>$LOGS_FILE
 dnf install nginx -y &>>$LOGS_FILE
-VALIDATE $? "Enable and start nginx module 20"
+VALIDATE $? "start nginx module 20"
 
 systemctl enable nginx &>>$LOGS_FILE
 systemctl start nginx  &>>$LOGS_FILE
@@ -47,6 +55,7 @@ VALIDATE $? "Download frontend content"
 cd /usr/share/nginx/html  &>>$LOGS_FILE
 unzip /tmp/frontend.zip &>>$LOGS_FILE
 VALIDATE $? "Extract the files from tmp to default page"
+
 
 cp /home/ec2-user/robo-shop/nginx.conf /etc/nginx/nginx.conf &>>$LOGS_FILE
 VALIDATE $? "Copying the nginx.conf"
